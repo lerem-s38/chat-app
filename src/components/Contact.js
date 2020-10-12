aeb4ed01adab5ed4a15270d7f749c7530b6aeafe
Rémi Cusset <56927808+lerem-s38@users.noreply.github.com>
2020-10-12 15:25:26 +0200

    stepDeux

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,22 +3,21 @@ import './Chat.css';
 
 function Contact() {
   const name = 'Morris Obrien';
-  const online = <span className='status-online'> </span>;
-  const offline = <span className='status-offline'></span>;
+  const online = true;
+  const statoffline = <span className='status-offline'>offline</span>;
+  const statonline = <span className='status-online'>online</span>;
 
   return (
-    <div className='Contact '>
+    <div className='Contact'>
       <img
-        className=' Contact avatar'
+        className='avatar'
         src='https://randomuser.me/api/portraits/men/81.jpg '
         alt='contact'
       />
-      <div className='Contact name'>
-        <p className='status'>
-          <h4>{name}</h4>
-
-          {online ? 'online' : 'offline!'}
-        </p>
+      <div className='status'>
+        <div className='name'>{name}</div>
+        <div className=' contact '></div>
+        <div className='status'>{online ? statonline : statoffline}</div>
       </div>
     </div>
   );
